059701d768800452872737d3e944a0b0b6053db0
4e1ovekER <bolcelovek518@gmail.com>
2026-02-20 19:26:31 +0500

    добавил докумкентацию

--- SimpleC++Snake/utilities.h
+++ SimpleC++Snake/utilities.h
@@ -15,7 +15,19 @@
 
 //******************Typedefs, Constexprs, & Defines******************
 #define IS_DOWN(b) inputs.buttons[b].is_down
+/**
+ * @def IS_DOWN(b)
+ * @brief Проверяет, зажата ли клавиша в текущем кадре
+ * @param b Индекс клавиши для проверки
+ * @return true если клавиша зажата
+ */
 #define PRESSED(b) inputs.buttons[b].is_down && inputs.buttons[b].changed
+ /**
+  * @def PRESSED(b)
+  * @brief Проверяет, была ли клавиша нажата в этом кадре
+  * @param b Индекс клавиши для проверки
+  * @return true если клавиша была нажата именно в этом кадре
+  */
 
 typedef unsigned int uint32;
 
@@ -46,19 +58,62 @@ constexpr uint32 VK_W = 0x57;
 constexpr uint32 VK_X = 0x58;
 constexpr uint32 VK_Y = 0x59;
 constexpr uint32 VK_Z = 0x5A;
+/**
+ * @brief Виртуальные коды клавиш A-Z (0x41-0x5A)
+ * @details Используются для обработки нажатий клавиш
+ */
+ /**
+  * @brief Ширина тайловой карты в количестве тайлов
+  */
 
 // tilemap
 constexpr int tilemapSizeX = 15;
 constexpr int tilemapSizeY = 15;
 
+/**
+ * @struct RenderState
+ * @brief Хранит состояние рендерера
+ * @var RenderState::width
+ *   Ширина окна в пикселях
+ * @var RenderState::height
+ *   Высота окна в пикселях
+ * @var RenderState::memory
+ *   Указатель на буфер пикселей
+ * @var RenderState::bitmap_info
+ *   Информация о битмапе для Windows
+ */
+
 // colors
 constexpr uint32 LIGHTGREEN = 0x007a21;
+/**
+ * @brief Светло-зеленый цвет для тайлов
+ */
 constexpr uint32 SLIGHTLYDARKGREEN = 0x00b515;
+/**
+ * @brief Чуть темнее зеленый для градиента
+ */
 constexpr uint32 DARKGREEN = 0x0f3814;
+/**
+ * @brief Темно-зеленый для фона
+ */
+
 constexpr uint32 DARKERGREEN = 0x003900;
+/**
+ * @brief Еще темнее зеленый для границ
+ */
 constexpr uint32 DARKESTGREEN = 0x002000;
+/**
+ * @brief Самый темный зеленый для шапки
+ */
 constexpr uint32 BEAUTIFULBLUE = 0x262afd;
+/**
+ * @brief Синий цвет для специальных тайлов
+ */
 constexpr uint32 APPLERED = 0xfc0140;
+/**
+ * @brief Красный цвет для специальных тайлов
+ */
+
 
 //******************Typedefs, Constexprs, & Defines******************
 
@@ -72,8 +127,26 @@ struct RenderState {
 
 //******************Functions*****************
 uint32 calculateTileColor(const int xPos, const int yPos);
+/**
+ * @brief Возвращает текущий FPS
+ * @return Количество кадров в секунду
+ */
 int getFPS();
+/**
+ * @brief Возвращает состояние тика для анимаций
+ * @return true если четный тик, false если нечетный
+ */
 bool getTickState();
+/**
+ * @brief Ограничивает значение между минимумом и максимумом
+ * @tparam T1 Тип минимального значения
+ * @tparam T2 Тип значения для ограничения
+ * @tparam T3 Тип максимального значения
+ * @param min Минимальное допустимое значение
+ * @param val Значение для ограничения
+ * @param max Максимальное допустимое значение
+ * @return Ограниченное значение
+ */
 
 
 template <typename T1, typename T2, typename T3> inline T2 clamp(T1 min, T2 val, T3 max) {
@@ -81,15 +154,37 @@ template <typename T1, typename T2, typename T3> inline T2 clamp(T1 min, T2 val,
 	else if (val > max) return static_cast<T2>(max);
 	else				return val;
 }
+/**
+ * @brief Текущее состояние рендерера для отрисовки
+ */
 //******************Functions******************
 
 //******************Global Variables******************
 extern RenderState renderer;
+/**
+ * @brief Флаг работы программы
+ * @details true - программа работает, false - программа завершается
+ */
 extern bool running;
+/**
+ * @brief Масштаб отрисовки всех элементов
+ */
 extern float scale;
+/**
+ * @brief Масштаб скорости по X для анимаций и движения
+ */
 extern float velocityScaleX;
+/**
+ * @brief Масштаб скорости по Y для анимаций и движения
+ */
 extern float velocityScaleY;
+/**
+ * @brief Двумерный массив [15][15] с цветами всех тайлов карты
+ */
 extern std::array<std::array<uint32, tilemapSizeY>, tilemapSizeX> tilemap;
+/**
+ * @brief Номер текущего запуска/кадра для анимаций
+ */
 extern uint32 runNumber;
 //******************Global Variables******************
 
